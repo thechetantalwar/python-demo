a5d9f98c251fc09470733189eb2a3e114f4a82f3
Chetan Talwar <thechetantalwar@users.noreply.github.com>
2021-11-16 11:08:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Hello and Welcome to Docker Workshop!"
+
+@app.route('/feedback')
+def hello():
+    return 'We aim for 5 star rating from our learners!'
+
+if __name__ == "__main__":
+    app.run()
